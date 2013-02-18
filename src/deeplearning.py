a1b541962c077cbe264a7a42faca390f1ae1089f
hiqdevlord <hiqdevlord@gmail.com>
2013-02-18 10:01:51 +0000

    Setting up

---
+++ src/deeplearning.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 18, 2013
+
+@author: Ash Booth
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ src/main.py
@@ -0,0 +1,8 @@
+'''
+Created on Feb 18, 2013
+
+@author: Ash Booth
+'''
+
+if __name__ == '__main__':
+    pass
