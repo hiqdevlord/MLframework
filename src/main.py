04e3ede95d3b5e0bd9f4c807cc08bcc387333fac
hiqdevlord <hiqdevlord@gmail.com>
2013-02-18 15:46:22 +0000

    Logistic regression implemented with Theano and working

--- src/main.py
+++ src/main.py
@@ -4,5 +4,11 @@ Created on Feb 18, 2013
 @author: Ash Booth
 '''
 
+import cPickle, gzip, numpy
+import deeplearning
+from logreg import LogisticRegression
+
+import theano.tensor as T
+
 if __name__ == '__main__':
     pass
